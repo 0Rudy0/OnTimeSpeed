8dcf740bc7239def6af9c71fb70acbbbea7b64ab
0Rudy0 <rudman0@gmail.com>
2020-02-22 14:35:15 +0100

    dfhsg

---
+++ OnTimeSpeed/Scripts/workAmount.js
@@ -0,0 +1,22 @@
+﻿function increaseWorkAmount() {
+    if (this.workAmount() <= 0)
+        this.workAmount(0);
+
+    this.workAmount(Math.min(this.workAmount() + 0.5), 8);
+}
+
+function decreaseWorkAmount() {
+    if (this.workAmount() >= 8)
+        this.workAmount(8);
+
+    this.workAmount(Math.max(0, this.workAmount() - 0.5));
+}
+
+function maxWorkAmount() {
+    if (this.workAmount() > 8) {
+        this.workAmount(0);
+    }
+    else {
+        this.workAmount(99);
+    }
+}
